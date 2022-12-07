a979c221b489b19b0749bd963f3e32c57e1338fb
cassiusbessa <cassiusbessa@gmail.com>
2022-12-06 21:29:53 -0300

    fix(services): :bug: changes url to next page

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import fetch from 'node-fetch';
 
 
 class Service {
@@ -6,15 +7,20 @@ class Service {
   constructor(private token = process.env.TOKEN, private baseUrl = process.env.BASE_URL) {
   }
 
-  private async request(endpoint: string, param: any) {
+  private async request(endpoint: string, param: string) {
     const res = await fetch(`${this.baseUrl}/${endpoint}?${param}`,
       { headers: { Authorization: `Bearer ${this.token}` } });
-    return await res.json();
+    const result = await res.json();
+    return { status: res.status, data: result}
   }
   
-  public async getUsers(since?: number, per_page?: number) {
-    return this.request('user', {since: since || 0, per_page: per_page || 50});    
+  public async getUsers(since?: string, per_page?: string) {
+    const {status, data} = await this.request('users', `since=${since || 0}&per_page=${per_page || 50}`);
+    const lastId = data.length ? data[data.length - 1].id : null;
+    const nextPage = lastId ? `http://localhost:3001/api/users?since=${lastId}&per_page=${per_page || 50}` : null;
+    return { status, data, nextPage }    
   }
 
-
 }
+
+export default new Service();
