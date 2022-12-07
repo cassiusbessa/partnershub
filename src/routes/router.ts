9f2243e80049160b233dfb06f55a0dd6c48700cf
cassiusbessa <cassiusbessa@gmail.com>
2022-12-06 21:52:11 -0300

    feat: :sparkles: implements getUser route

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import controllers from '../controllers';
 const router = Router();
 
-router.use('/api/users', controllers.getUsers);
+router.get('/api/users', controllers.getUsers);
+router.get('/api/users/:username/details', controllers.getUser);
 
 export default router;
