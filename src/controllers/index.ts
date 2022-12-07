9c4af0627ece467706f13a1d8649a629bfb46bc7
cassiusbessa <cassiusbessa@gmail.com>
2022-12-06 21:51:22 -0300

    feat(controllers): :sparkles: implements getUser method

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -9,6 +9,13 @@ class Controller {
     const { status, data, nextPage }  = await Service.getUsers(since as string, per_page as string);
     return res.status(status).json({users: data, nextPage});
   }
+
+  public async getUser (req: Request, res: Response) {
+    const { username } = req.params;
+    const { status, data }  = await Service.getUser(username);
+    return res.status(status).json(data);
+  }
+
 }
 
 export default new Controller();
