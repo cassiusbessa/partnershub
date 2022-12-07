01bb8afda84cb754582b4407e6bb55eaf36d6c7e
cassiusbessa <cassiusbessa@gmail.com>
2022-12-06 21:57:35 -0300

    feat: :sparkles: implements getUserRepo route

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,5 +4,6 @@ const router = Router();
 
 router.get('/api/users', controllers.getUsers);
 router.get('/api/users/:username/details', controllers.getUser);
+router.get('/api/users/:username/repos', controllers.getUserRepos);
 
 export default router;
